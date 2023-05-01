a3240b52cd97c806c5766c1b4ff4483955bcafc0
henishakachhadiya <henishakachhadiya7@gmail.com>
2023-05-01 18:40:35 +0530

    test

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+      return(
+        <>
+            <h1 className='demo'>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Welcome</h1>
+        </>
+       
+
+      )
+}
+
+export default App;
